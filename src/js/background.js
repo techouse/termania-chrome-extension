d4cf169ba3602fad25e661ffc175ec44bf080d60
Klemen Tušar <techouse@gmail.com>
2020-01-08 17:47:53 +0100

    :bug: Fetch dictionaries on setup

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,12 @@
 import install                        from "@/install"
 import context, { createContextMenu } from "@/context"
+import { getDictionaries }            from "@/services"
 
 /**
  * Install the database if needed
  */
 chrome.runtime.onInstalled.addListener(() => {
+    getDictionaries()
     install()
     createContextMenu()
 })
