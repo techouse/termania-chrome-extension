c3a1ca47e1fe1daad1e7b2e7fb42fca5a2b992e5
Klemen Tušar <techouse@gmail.com>
2020-01-12 23:25:53 +0000

    :arrow_up: Upgrade database on extension upgrade

--- src/js/background.js
+++ src/js/background.js
@@ -5,23 +5,27 @@ import { DEFAULT_DICTIONARY_ID } from "@/services/constants"
 
 chrome.runtime.onInstalled.addListener(() => {
     import(/* webpackChunkName: "services" */ "@/services").then(({ getDictionaries }) => {
-        getDictionaries().then(() => {
-            import(/* webpackChunkName: "dictionary" */ "@/models/Dictionary").then(({ default: Dictionary }) => {
-                Dictionary.getActive()
-                          .catch(() => {
-                              chrome.storage.local.get("dictionaries", ({ dictionaries }) => Dictionary.setActive(dictionaries.dictionaries.find((dict) => dict.id === DEFAULT_DICTIONARY_ID)))
-                          })
+        getDictionaries()
+            .then(() => {
+                import(/* webpackChunkName: "dictionary" */ "@/models/Dictionary").then(({ default: Dictionary }) => {
+                    Dictionary.getActive()
+                              .catch(() => {
+                                  chrome.storage.local.get("dictionaries", ({ dictionaries }) => Dictionary.setActive(dictionaries.dictionaries.find((dict) => dict.id === DEFAULT_DICTIONARY_ID)))
+                              })
+                })
             })
-        })
     })
 
-    import(/* webpackChunkName: "install" */ "@/install").then(({ default: install }) => {
-        install()
+    import(/* webpackChunkName: "install" */ "@/install").then(({ isOutdated, default: install }) => {
+        isOutdated()
+            .then(() => {
+                console.log("Database is outdated or does not exist!")
+                install()
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
     })
-
-    // import(/* webpackChunkName: "rebuild" */ "@/rebuild").then(({ default: rebuild }) => {
-    //     rebuild(chrome.runtime.getURL("data/sloleks.json"))
-    // })
 
     import(/* webpackChunkName: "context" */ "@/context").then(({ createContextMenu }) => {
         createContextMenu()
